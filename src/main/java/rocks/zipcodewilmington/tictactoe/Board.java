407850dd425d86ce3543ce182fb56ddc846349e5
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-11-04 23:22:23 -0400

    almost completed lab

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -1,5 +1,7 @@
 package rocks.zipcodewilmington.tictactoe;
 
+import java.io.StringReader;
+
 /**
  * @author leon on 6/22/18.
  */
@@ -12,65 +14,53 @@ public class Board {
         this.matrix = matrix; // initialized instance variable
     }
 
-    public static void main(String[] args) {
 
-        Character[][] board = {
-                {'X', 'X', 'O'},
-                {'O', 'O', 'X'},
-                {'O', 'O', 'X'}
-        };
-            /*   example
-     {           col
-             row{'X', 'O', 'X'},
-                {'O', 'O', 'X'},
-                {'X', 'X', 'O'}
-        }
-      */
-
-        Boolean isInFavorOfX = null;
-        Integer playerX = 0;
-        Integer playerO = 0;
-
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[row].length; col++) {
-                if (board[row][col] == 'X') {
-                    playerX++;
-
-                } else {
-                    playerO++;
-                }
-            }
-        }
-
-        System.out.println("O :"+playerO);
-        System.out.println("X :"+playerX);
-
-    }
-
-// work in progress
-
+// work in progress   MADE SOME PROGRESS
+   
     public Boolean isInFavorOfX() {
-      Character[][] board = this.matrix;
+
             /*   example
      {           col
              row{'X', 'O', 'X'},
                 {'O', 'O', 'X'},
                 {'X', 'X', 'O'}
+
+
+
+           Ways to win
+           row1 [0][0],[0][1],[0][2]
+           row2 [1][0],[1][1],[1][2]
+           row3 [2][0],[2][1],[2][2]
+           --------------------------
+           col1 [0][0],[1][0],[2][0]
+           col1 [0][1],[1][1],[2][1]
+           col1 [0][2],[1][2],[2][2]
+           ----------------------------
+           diag1[0][0],[1][1],[2][2]
+           diag2[0][2],[1][1],[2][0]
         }
       */
 
         Boolean isInFavorOfX = false;
-        Integer playerX = 0; //Keeps track of player X turns
-        Integer playerO = 0; //Keeps track of player O turns
 
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[row].length; col++) {
-                if (board[row][col] == 'X') {
-                    playerX++;
 
-                } else {
-                    playerO++;
-                }
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+
+                   if(matrix[row][0] == 'X' && matrix[row][1] == 'X' && matrix[row][2] == 'X'){//horizontal win
+                    isInFavorOfX = true;
+
+              }else if(matrix[0][col] == 'X' && matrix[1][col] == 'X' && matrix[2][col] == 'X'){//vertical win
+                    isInFavorOfX =true;
+
+              }else if(matrix[row][0] == 'X' && matrix[row][1] == 'X' && matrix[row][2] == 'X'){//diagonal win
+                  isInFavorOfX = true;
+
+              }else if(matrix[row][2] == 'X' && matrix[row][1] == 'X' && matrix[row][0] == 'X'){
+                  isInFavorOfX =true;
+
+              }
+
             }
         }
 
@@ -81,18 +71,51 @@ public class Board {
 
     public Boolean isInFavorOfO() {
 
-        return null;
+        Boolean isInFavorOfO = false;
+
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+
+                if(matrix[row][0] == 'O' && matrix[row][1] == 'O' && matrix[row][2] == 'O'){
+                    isInFavorOfO = true;
+
+                }else if(matrix[0][col] == 'O' && matrix[1][col] == 'O' && matrix[2][col] == 'O'){
+                    isInFavorOfO =true;
+
+                }else if(matrix[row][0] == 'O' && matrix[row][1] == 'O' && matrix[row][2] == 'O'){
+                    isInFavorOfO = true;
+
+                }else if(matrix[row][2] == 'O' && matrix[row][1] == 'O' && matrix[row][0] == 'O'){
+                    isInFavorOfO =true;
+
+                }
+
+            }
+        }
+
+
+        return isInFavorOfO;
     }
 
     public Boolean isTie() {
 
 
-        return null;
+        return (isInFavorOfO() || isInFavorOfX());
     }
 
     public String getWinner() {
+        String winner = "";
+       if(isInFavorOfX()){
+          winner = "X";
 
-        return "X";
+       }else if(isInFavorOfO()){
+          winner = "O";
+
+       }
+
+
+        return winner;
     }
 
 }
