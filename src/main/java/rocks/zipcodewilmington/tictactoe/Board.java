029fa7c942ace317413bc4230a85d7fb359d94ac
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-11-04 21:49:58 -0400

    working on it

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,95 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
+
+
+    private Character[][] matrix; // created instance variable
+
+    public Board(Character[][] matrix) { // constructor
+        this.matrix = matrix; // initialized instance variable
     }
+
+    public static void main(String[] args) {
+
+        Character[][] board = {
+                {'X', 'X', 'O'},
+                {'O', 'O', 'X'},
+                {'O', 'O', 'X'}
+        };
+            /*   example
+     {           col
+             row{'X', 'O', 'X'},
+                {'O', 'O', 'X'},
+                {'X', 'X', 'O'}
+        }
+      */
+
+        Boolean isInFavorOfX = null;
+        Integer playerX = 0;
+        Integer playerO = 0;
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                if (board[row][col] == 'X') {
+                    playerX++;
+
+                } else {
+                    playerO++;
+                }
+            }
+        }
+
+        System.out.println("O :"+playerO);
+        System.out.println("X :"+playerX);
+
+    }
+
+// work in progress
 
     public Boolean isInFavorOfX() {
-        return null;
+      Character[][] board = this.matrix;
+            /*   example
+     {           col
+             row{'X', 'O', 'X'},
+                {'O', 'O', 'X'},
+                {'X', 'X', 'O'}
+        }
+      */
+
+        Boolean isInFavorOfX = false;
+        Integer playerX = 0; //Keeps track of player X turns
+        Integer playerO = 0; //Keeps track of player O turns
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                if (board[row][col] == 'X') {
+                    playerX++;
+
+                } else {
+                    playerO++;
+                }
+            }
+        }
+
+
+        return isInFavorOfX;
     }
 
+
     public Boolean isInFavorOfO() {
+
         return null;
     }
 
     public Boolean isTie() {
+
+
         return null;
     }
 
     public String getWinner() {
-        return null;
+
+        return "X";
     }
 
 }
